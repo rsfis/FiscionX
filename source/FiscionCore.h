2dff0dec4544a09cfb131f5bb25dfc1e9e79f508
rsfis <rsfiscina01@gmail.com>
2025-08-19 20:39:06 -0300

    Changed struct to namespace

--- source/FiscionCore.h
+++ source/FiscionCore.h
@@ -195,7 +195,7 @@ struct File {
 	void saveFile(std::string outputPath);
 };
 
-struct FiscionX {
+namespace FiscionX {
 	struct Vector3 {
 		float x, y, z;
 		Vector3(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
